bfa2f60228970aa158c232568eb65e8c1caf270f
Arnav Podichetty <arnav.p2024@gmail.com>
2025-04-30 16:35:58 -0700

    Initial Commit

---
+++ Emotio/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Emotio
+//
+//  Created by Arnav Podichetty on 4/30/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Emotio/EmotioApp.swift
@@ -0,0 +1,17 @@
+//
+//  EmotioApp.swift
+//  Emotio
+//
+//  Created by Arnav Podichetty on 4/30/25.
+//
+
+import SwiftUI
+
+@main
+struct EmotioApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
